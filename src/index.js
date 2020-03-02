8795651813437b582b8d4f706da8f00caaaa56ed
Konstantin Dedulya <eximal@gmail.com>
2020-03-02 23:24:56 +0300

    feat: add solution for the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let bracketsObj = {}
+  let bracketsStack = [];
+  let arr = str.split('');
+
+  bracketsConfig.forEach(item => {
+      bracketsObj[item[0]] = item[1];
+  });
+
+  for(let i = 0; i < arr.length; i++) {
+      if (arr[i] === bracketsObj[bracketsStack[bracketsStack.length - 1]]) {
+          bracketsStack.pop();
+      } else if (Object.keys(bracketsObj).includes(arr[i])) {
+          bracketsStack.push(arr[i]);
+      } else return false;
+  }
+  
+  if (bracketsStack.length === 0) return true;
+  return false;
 }
